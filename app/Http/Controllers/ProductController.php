b689094d8652cee98fcb9d15f0a1669fb33dac0d
anannnnnnnnnnnnnn <anantohtia010746@gmail.com>
2024-01-19 16:03:54 +0700

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        $request = [ 'name' => 'index','payload' =>Product::all()];
+        return $request;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+      $paylord = Product::find($id);
+      return['name' => 'show','paylord'=>$paylord];
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
